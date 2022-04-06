baadbfc53df328aae75065e98ee412b5626591b0
aavarghese <avarghese@us.ibm.com>
2022-04-06 14:11:19 -0400

    WIP Unit tests and github action
    Signed-off-by: aavarghese <avarghese@us.ibm.com>

---
+++ src/test/java/gdb/mi/service/command/TestMIBreakDeleteCommand.java
@@ -0,0 +1,18 @@
+package gdb.mi.service.command;
+
+import gdb.mi.service.command.commands.MIBreakDelete;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TestMIBreakDeleteCommand {
+
+	@Test
+	public void deleteSingleBreakpoint() {
+		MIBreakDelete target = new MIBreakDelete(new String[]{"1"});
+
+		assertEquals("", "-break-delete 1\n",
+				target.constructCommand());
+	}
+
+}
